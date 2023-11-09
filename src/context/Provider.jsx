1f58c443bb84fb09f2f0a0c0d87a9db2030b73b7
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-09 17:42:22 +0100

    add the google search engine endpoint and set up it with axios

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from 'react'
 import SearchContext from "./SearchContext";
+import axios from 'axios';
+
+const baseUrl = 'https://google-search72.p.rapidapi.com/'
 
 const Provider = ({ children }) => {
   const [search, setSearch] = useState("Nike");
+  const [pathEndPoint, setPathEndPoint] = useState('/search');
+
+  const searchResult = () => {
+    axios.get(`${baseUrl}${pathEndPoint}`, {
+      headers: {
+        "Content-Type": "application/jso",
+        "X-RapidAPI-Host": "google-search72.p.rapidapi.com",
+        "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+      },
+      params: {
+        q: search
+      }
+    })
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
   return (
     <SearchContext.Provider value={{ search, setSearch }}>
